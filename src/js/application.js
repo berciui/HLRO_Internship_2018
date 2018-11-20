133f899ecd24610685230af45cad7f7a7de6ba18
RadomirO <oana-violeta.radomir@haufe-lexware.com>
2018-11-20 10:59:11 +0200

    modified application.js to try and implement enter search

--- src/js/application.js
+++ src/js/application.js
@@ -1,3 +1,19 @@
-function initEvents(){
-    $(searchMoviesButton).onClick()
-}
+$(function() {
+    $("#searchMovieBtn").click( function()
+         {
+             if($("#movieTitleInput").val()){
+                alert($("#movieTitleInput").val());
+             }
+             else{
+                alert('No value entered for movie title :(');
+             }
+         }
+    );
+    var input= document.getElementById("searchMovieBtn")
+    input.addEventListener("keyup", function(event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            document.getElementById("searchMovieBtn").click();
+  }
+});
+});
