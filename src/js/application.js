6bbb470dbcf43e3c4d17abd6931f555ec6924690
Batrinu <BatrinuR@haufe.com>
2018-11-20 12:15:06 +0200

    text + svg

--- src/js/application.js
+++ src/js/application.js
@@ -1,3 +1,11 @@
-function initEvents(){
-    $ (searchMoviesButton).onClick()
-}
+$('body').on('keypress', 'movieTitleInput', function(e) {
+    if (e.keyCode == 13) {
+        $("#movieTitleInput").click(){
+            alert($("#searchMovieBtn").val());
+        };
+        else {
+            alert('No value entered for movie title :(');
+        }
+        
+    }
+});
