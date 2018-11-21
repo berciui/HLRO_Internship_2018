047c2a3e70f4d371836ddc95401e413c2f43d14f
Batrinu <BatrinuR@haufe.com>
2018-11-21 14:57:27 +0200

    Changes to structure

--- src/js/application.js
+++ src/js/application.js
@@ -1,17 +1,25 @@
 $(function () {
-   $("#searchMovieBtn").click(function () {
-      if ($("#movieTitleInput").val()) {
-         alert($("#movieTitleInput").val());
-      }
-      else {
-         alert('No value entered for movie title :(');
-      }
-   }
-   );
+    // Bind click to Search
+    $("#searchMovieBtn").click(handleSearch);
 
-   $( "#movieTitleInput" ).on( "keydown", function(event) {
-      if(event.which == 13){
-         alert($("#movieTitleInput").val());
-      }
+    $("#movieTitleInput").on("keydown", function (event) {
+        if (event.which == 13) {
+            handleSearch();
+        }
     });
 });
+
+function handleSearch(){
+    let movieTitle = $("#movieTitleInput").val();
+    if (movieTitle) {
+        $.get("http://www.omdbapi.com/?apikey=e373dab5&s=" + movieTitle)
+            .done(processMovieListFromServer);
+    }
+    else {
+        alert('Enter movie title to search for!');
+    }
+}
+
+function processMovieListFromServer(data){
+    console.log(data);
+}
