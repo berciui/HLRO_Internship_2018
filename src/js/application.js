674749f513450b3a5cff8eb12ac10cdd76362c3f
Berciu, Ioan <Ioan.Berciu@haufe-lexware.com>
2018-11-21 12:30:57 +0200

    Client Server Communication

--- src/js/application.js
+++ src/js/application.js
@@ -1,27 +1,16 @@
-$(function() {
+$(function () {
     // bind click event on the search button
-    $("#searchMovieBtn").click( function()
-         {
-             if($("#movieTitleInput").val()){
-                alert($("#movieTitleInput").val());
-             }
-             else{
-                alert('No value entered for movie title :(');
-             }
-         }
+    $("#searchMovieBtn").click(function () {
+        let movieTitle = $("#movieTitleInput").val();
+        if (movieTitle) {
+            $.get("http://www.omdbapi.com/?apikey=__API_KEY__&s=" + movieTitle)
+                .done(function (data) {
+                    console.log(data); 
+                });
+        }
+        else {
+            alert('No value entered for movie title :(');
+        }
+    }
     );
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
 });
