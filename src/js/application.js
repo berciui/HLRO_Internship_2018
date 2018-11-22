38ef38fe7d475350531ec53da451249f8f10786a
Batrinu <BatrinuR@haufe.com>
2018-11-22 12:01:17 +0200

    Changes to: index, style.css, template & application

--- src/js/application.js
+++ src/js/application.js
@@ -13,25 +13,12 @@ function handleSearch(){
     let movieTitle = $("#movieTitleInput").val();
     if (movieTitle) {
         $.get("http://www.omdbapi.com/?apikey=e373dab5&s=" + movieTitle)
-            .done(processMovieListFromServer);
+            .done(function (getData){
+                console.log (getData);
+                $("movieList").loadTemplate("src/js/Template.movie.html", getData.Search, {});
+            });
     }
     else {
-        alert('Enter movie title to search for!');
+        alert('Please enter movie title to search for!');
     }
-}
-
-function processMovieListFromServer(data){
-    console.log(data);
-    $.each(data.Search, function(movieData){
-        let moviInfo = "<div class=\"card\" style=\"width: 18rem;\">\
-        <img class=\"card-img-top\" src=\"" + movieData.Poster  + "\"\
-            alt=\"Card image cap\">\
-        <div class=\"card-body\">\
-            <h5 class=\"card-title\">" + movieData.Title + "</h5>\
-            <h6>" + movieData.Year +"</h6>\
-            <p class=\"card-text\"></p>\
-            <a href=\"#\" class=\"btn btn-primary\">Go to IMDb</a>\
-        </div>";
-        console.log(moviInfo);
-    });
 }
