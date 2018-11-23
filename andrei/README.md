c99a1f78b5cb3b7e0086a98a3c0d56baf7a1113a
Berciu, Ioan <Ioan.Berciu@haufe-lexware.com>
2018-11-23 17:09:24 +0200

    Add Andrei's work to master as its own folder to be able to display it as static page

---
+++ andrei/README.md
@@ -0,0 +1,3 @@
+# HLRO_Internship_2018
+
+### Clone or download the repo and open docs/Welcome.html in your browser 
---
+++ andrei/src/js/application.js
@@ -0,0 +1,27 @@
+$(function () {
+    // bind click event on the search button
+    $("#searchMovieBtn").click(handleSearch);
+    $("#movieTitleInput").on("keydown", function (event) {
+        if (event.which == 13) {
+            handleSearch();
+        }
+    });
+
+});
+
+function handleSearch() {
+    let movieTitle = $("#movieTitleInput").val();
+    if (movieTitle) {
+        $.get("https://www.omdbapi.com/?apikey=1c17ad99&s=" + movieTitle)
+        .done(function (serverResponseToGetRequest) {
+            console.log(serverResponseToGetRequest);
+            let movieListReturnedByServer = serverResponseToGetRequest.Search;
+            console.log(movieListReturnedByServer);
+            $("#moviesList").loadTemplate("src/js/movieItemTemplate.html", movieListReturnedByServer, {});
+        });
+}
+    else {
+        alert('No value entered for movie title :(');
+    }
+
+}
